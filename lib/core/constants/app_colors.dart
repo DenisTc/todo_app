82f1fdf45f4195edba8c50fedcdb5aac6678ed50
DenisTc <TsapaDe@yandex.ru>
2022-07-25 23:40:31 +0300

    Добавил svg иконки

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -34,21 +34,4 @@ abstract class AppColors {
   static const backPrimaryDark = Color(0xFF161618);
   static const backSecondaryDark = Color(0xFF252528);
   static const backElevatedDark = Color(0xFF3C3C3F);
-
-  // static const scaffoldBackgroundColor = Color(0xFFE5E5E5);
-  // static const separator = Color(0x33000000);
-  // static const overlay = Color(0x0F000000);
-  // static const primary = Color(0x00000000);
-  // static const secondary = Color(0x99000000);
-  // static const tertiary = Color(0x4D000000);
-  // static const disable = Color(0x26000000);
-  // static const red = Color(0xFFFF3B30);
-  // static const green = Color(0xFF34C759);
-  // static const blue = Color(0xFF007AFF);
-  // static const gray = Color(0xFF8E8E93);
-  // static const lightGray = Color(0xFFD1D1D6);
-  // static const white = Color(0xFFFFFFFF);
-  // static const primaryLight = Color(0xFFF7F6F2);
-  // static const secondaryLight = Color(0xFFFFFFFF);
-  // static const elevatedLight = Color(0xFFFFFFFF);
 }
---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,8 @@
+abstract class AppIcons {
+  static const String visibilityOn = 'assets/icons/visibility_on.svg';
+  static const String visibilityOff = 'assets/icons/visibility_off.svg';
+  static const String infoOutline = 'assets/icons/info_outline.svg';
+  static const String check = 'assets/icons/check.svg';
+  static const String delete = 'assets/icons/delete.svg';
+  static const String close = 'assets/icons/close.svg';
+}
